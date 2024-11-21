8315f05df057a66542008ad4488575ff50bedbc5
Rohit Kumar Yadav <rohit.yadav102002@gmail.com>
2024-11-21 15:01:50 +0530

    add

--- src/Pages/Project/index.js
+++ src/Pages/Project/index.js
@@ -55,7 +55,7 @@ const Project = () => {
           url="https://entertainment-ui.netlify.app/"
           imageName="entertainment"
           title="Entertainment Webpage"
-          techStack={"Technogy: HTML, CSS, JavaScript, Bootstrap, Dreamviewer"}
+          techStack={"Technogy: HTML, CSS, JavaScript, Bootstrap"}
           module1="Introduction/About Webpage"
           module2={"Seamless navigation"}
           module3={"Interconnected Web Pages"}
